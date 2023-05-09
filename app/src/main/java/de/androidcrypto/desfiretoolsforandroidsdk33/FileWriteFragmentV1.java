f6f17a675f73f83cb871e15a1cd31adcdd09f346
FlutterCrypto <fluttercrypto@gmx.de>
2023-05-09 22:04:21 +0200

    updated

--- app/src/main/java/de/androidcrypto/desfiretoolsforandroidsdk33/FileWriteFragmentV1.java
+++ app/src/main/java/de/androidcrypto/desfiretoolsforandroidsdk33/FileWriteFragmentV1.java
@@ -5,22 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.github.skjolber.desfire.ev1.model.DesfireApplication;
 import com.github.skjolber.desfire.ev1.model.file.DesfireFile;
-import com.shawnlin.numberpicker.NumberPicker;
 
 /**
  * class added by MichaelsPlayground / AndroidCrypto
@@ -28,21 +21,20 @@ import com.shawnlin.numberpicker.NumberPicker;
 
 // todo use Bundle instead of a constructor for parameter
 @SuppressLint("ValidFragment")
-public class FileWriteFragment extends Fragment {
+public class FileWriteFragmentV1 extends Fragment {
 
 	// todo block file creation if application id = 00 00 00 = master file application
 
-	private LinearLayout nonValueLayout, valueLayout;
-	private EditText fileId, fileType, dataToWrite, creditValueWrite, debitValueWrite;
+	private EditText fileId, fileType, dataToWrite;
 	private TextView logData;
-	private Button writeToFile, writeCredit, writeDebit;
+	private Button writeToFile;
 	DesfireFile file;
 
-	private View.OnClickListener listener, creditListener, debitListener;
+	private View.OnClickListener listener;
 
 	// todo use Bundle instead of a constructor for parameter
 	@SuppressLint("ValidFragment")
-	public FileWriteFragment(DesfireFile file) {
+	public FileWriteFragmentV1(DesfireFile file) {
 		this.file = file;
 	}
 
@@ -57,23 +49,11 @@ public class FileWriteFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_file_write, container, false);
 
-		nonValueLayout = view.findViewById(R.id.llFileWriteNonValueLayout);
-		valueLayout = view.findViewById(R.id.llFileWriteValueLayout);
-
 		fileId = view.findViewById(R.id.etFileWriteFileId);
 		fileType = view.findViewById(R.id.etFileWriteFileType);
-
 		dataToWrite = view.findViewById(R.id.etFileWriteData);
 		writeToFile = view.findViewById(R.id.btnWriteToFile);
 		writeToFile.setOnClickListener(listener);
-
-		creditValueWrite = view.findViewById(R.id.etFileWriteCreditValue);
-		writeCredit = view.findViewById(R.id.btnFileWriteCredit);
-		writeCredit.setOnClickListener(creditListener);
-		debitValueWrite = view.findViewById(R.id.etFileWriteDebitValue);
-		writeDebit = view.findViewById(R.id.btnFileWriteDebit);
-		writeDebit.setOnClickListener(debitListener);
-
 		logData = view.findViewById(R.id.tvLog);
         return view;
     }
@@ -88,13 +68,6 @@ public class FileWriteFragment extends Fragment {
 	private void initFileData() {
 		fileId.setText(file.getIdString());
 		fileType.setText(file.getFileType().toString());
-		if (file.getFileType().toString().equals("VALUE_FILE")) {
-			nonValueLayout.setVisibility(View.GONE);
-			valueLayout.setVisibility(View.VISIBLE);
-		} else {
-			nonValueLayout.setVisibility(View.VISIBLE);
-			valueLayout.setVisibility(View.GONE);
-		}
 	}
 
 	public void setFileId(String fileIdString) {
@@ -111,22 +84,6 @@ public class FileWriteFragment extends Fragment {
 
 	public void setOnClickListener(View.OnClickListener listener) {
 		this.listener = listener;
-	}
-
-	public String getCreditDataToWrite() {
-		return creditValueWrite.getText().toString();
-	}
-
-	public void setCreditOnClickListener(View.OnClickListener creditListener) {
-		this.creditListener = creditListener;
-	}
-
-	public String getDebitDataToWrite() {
-		return debitValueWrite.getText().toString();
-	}
-
-	public void setDebitOnClickListener(View.OnClickListener debitListener) {
-		this.debitListener = debitListener;
 	}
 
 	public void setLogData(String message) {
